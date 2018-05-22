fe894a65bd1c77c09368dbf01d92d349baa6e286
Aaron Moss <mossaaron@outlook.com>
2018-05-22 17:07:55 +0100

    git bash

--- app_aaron.py
+++ app_aaron.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pickle as pkl
 
 import datetime as dt
 import pandas as pd
@@ -10,36 +11,36 @@ import plotly.graph_objs as go
 
 
 app = dash.Dash()
-
-
-external_css = [ "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
-        "//fonts.googleapis.com/css?family=Raleway:400,300,600",
-        "https://codepen.io/ygkubrick/pen/KoZPvz.css", #this is the header formatting
-        "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
-
-for css in external_css:
-    app.css.append_css({ "external_url": css })
-
-external_js = [ "https://code.jquery.com/jquery-3.2.1.min.js",
-        "https://codepen.io/plotly/pen/KmyPZr.js" ]
-
-for js in external_js:
-    app.scripts.append_script({ "external_url": js })
-
-#app.css.append_css({'external url': 'https://codepen.io/aaron-moss/pen/GdeQyK.css'})
-
-colors = {'background': '#ED3131'}
-
-df_table = pd.read_csv('test_table.csv')
-df_symbol = pd.read_csv('tickers.csv')
-
+app.css.append_css({'external_url': 'https://codepen.io/aaron-moss/pen/erXjxa.css'})
 app.config['suppress_callback_exceptions']=True 
 
-def generate_table(dataframe, max_rows=10):
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Read in DATA
+#---------------------------------------------------------------------------------------------------------------------------
+
+df_table = pd.read_pickle('top_movers.pkl')
+df_table = df_table.tail(25)
+df_table = df_table[['symbol','close','5day_price','5day%_change']]
+df_table = df_table.iloc[::-1]
+
+
+df_clean_prices = pd.read_pickle('clean_prices.pkl')
+ticker_list = df_clean_prices.index.levels[0].unique()
+ticker_list = ticker_list.tolist()
+
+timespan_values = [5,7,10,25,50,100,150,365,730,1095,1460]  # Values allowed in the Timespan dropdown
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Generate Tables
+#---------------------------------------------------------------------------------------------------------------------------
+
+def generate_table(dataframe, max_rows=25):
     return html.Table(
         # Header
+
         [html.Tr([html.Th(col) for col in dataframe.columns])] +
 
         # Body
@@ -48,229 +49,188 @@ def generate_table(dataframe, max_rows=10):
         ]) for i in range(min(len(dataframe), max_rows))]
     )
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}[
-    
-    html.Div([
-    html.H2('Stock Explorer',
-                style={'display': 'inline',
-                       'float': 'center',
-                       'font-size': '2.65em',
-                       'margin-left': '7px',
-                       'font-weight': 'bolder',
-                       'font-family': 'Product Sans',
-                       'color': "rgba(117, 117, 117, 0.95)",
-                       'margin-top': '20px',
-                       'margin-bottom': '0'
-                       }
-        ),
+#---------------------------------------------------------------------------------------------------------------------------
+# Main Layout
+#---------------------------------------------------------------------------------------------------------------------------
 
+app.layout = html.Div([
+    
+    # Page Title
+    html.Div([                   
+    html.H2('Stock Explorer'),
     ],
     className='row'
     ),
 
+    # Dropdown List of Tickers and Timespan
     html.Div(
         [
         dcc.Dropdown(
             id='stock-ticker-input',
-            options=[{'label': s[0], 'value': str(s[1])}
-                     for s in zip(df_symbol.Company, df_symbol.Symbol)],
-            value=['GOOGL'],
+            options=[{'label': s, 'value': s}
+                    for s in ticker_list],
+                     
+            value=['AAPL'],
             multi=True
+        ),
+        html.Label('Display how many days of data?'),
+        dcc.Dropdown(
+            id='stock_timespan',
+            options=[{'label': s, 'value': s}
+                    for s in timespan_values],
+            value=['25'],
+            multi=False
         ),
         ],
         className='row'
     ),
 
-
+    # Graphs
     html.Div(
         className="row",
         children=[
-           
+            html.Div(
+                className="four columns",
+                children=html.Div([
+                    html.Label('TOP MOVERS'),
+                    generate_table(df_table)  # Table of Top Movers
+                    
+            ])               
+            ),
             html.Div(
                 className="eight columns",
                 children=html.Div([
-                    dcc.Graph(id='graph1'),
-                    dcc.Graph(id='graph2')
+                    dcc.Graph(id='graph1'),   # Candlestick 
+                    dcc.Graph(id='graph3'),   # Volume
+                    dcc.Graph(id='graph2')    # Moving Avg
        
             ])
             ),
             
-            html.Div(
-                className="four columns",
-                children=html.Div([
-                    generate_table(df_table),
-                    dcc.Graph(id='graph3')
-            ])               
-            ),
+
         ]
     ),
 ])
 
+#---------------------------------------------------------------------------------------------------------------------------
+# Plot Graph 1 - CANDLESTICK
+#---------------------------------------------------------------------------------------------------------------------------
 
 @app.callback(
     dash.dependencies.Output('graph1', 'figure'),
-    [dash.dependencies.Input('stock-ticker-input', 'value')])
+    [dash.dependencies.Input('stock-ticker-input', 'value'),
+     dash.dependencies.Input('stock_timespan', 'value')])
+def update_graph1(tickers,stock_timespan):
+       
+    df = df_clean_prices.loc[tickers]
+    df = df.reset_index()  
 
-def update_graph1(tickers):
-    #print(ticker)
-   
-    for i, ticker in enumerate(tickers):
-        try:
-            df = DataReader(str(ticker), 'morningstar',
-                            dt.datetime(2017, 1, 1),
-                            dt.datetime.now()).reset_index()
-        except:
-            graph1.append(html.H3(
-                'Data is not available for {}'.format(ticker),
-                style={'marginTop': 20, 'marginBottom': 20}
-            ))
-            continue
+    try:
+        days = int(stock_timespan[0])
+    except:
+        days = int(stock_timespan)
 
-        candlestick = [{
-            'x': df['Date'],
-            'open': df['Open'],
-            'high': df['High'],
-            'low': df['Low'],
-            'close': df['Close'],
-            'type': 'candlestick',
-            #'name': 'graph1',
-            'name': ticker,
-            'legendgroup': ticker,
-            'increasing': {'line': {'color': '#228B22'}},
-            'decreasing': {'line': {'color': '#FF0000'}}
-        }]
+    candlestick = [{
+         'x': df['date'][-days:],        
+         'open': df['open'][-days:],
+         'high': df['high'][-days:],
+         'low': df['low'][-days:],
+         'close': df['close'][-days:],
+         'type': 'candlestick',
+    }]
+
         
     return{
-            'data': candlestick# + bollinger_traces
-            #x = [1,2,3,4,5],
-            #y = [1,2,3,4,5],
-            #mode = 'markers')]
-    }
-   
-
-
-@app.callback(
-    dash.dependencies.Output('graph4', 'figure'),
-    [dash.dependencies.Input('stock-ticker-input', 'value')])
-def update_graph1(tickers):
-    #print(ticker)
-   
-    for i, ticker in enumerate(tickers):
-        try:
-            df = DataReader(str(ticker), 'morningstar',
-                            dt.datetime(2017, 1, 1),
-                            dt.datetime.now()).reset_index()
-        except:
-            graph4.append(html.H3(
-                'Data is not available for {}'.format(ticker),
-                style={'marginTop': 20, 'marginBottom': 20}
-            ))
-            continue
-
-        candlestick = [{
-            'x': df['Date'],
-            'open': df['Open'],
-            'high': df['High'],
-            'low': df['Low'],
-            'close': df['Close'],
-            'type': 'candlestick',
-            #'name': 'graph1',
-            'name': ticker,
-            'legendgroup': ticker,
-            'increasing': {'line': {'color': '#228B22'}},
-            'decreasing': {'line': {'color': '#FF0000'}}
-        }]
-        
-    return{
-            'data': candlestick# + bollinger_traces
-            #x = [1,2,3,4,5],
-            #y = [1,2,3,4,5],
-            #mode = 'markers')]
+        'data': candlestick,
+        'layout': {
+            'xaxis':{'rangeslider': {'visible': False}},
+            'title' : 'Candlestick'
+        }
     }
 
+#---------------------------------------------------------------------------------------------------------------------------
+# Plot Graph 2 - VOLUME
+#---------------------------------------------------------------------------------------------------------------------------
 
 @app.callback(
     dash.dependencies.Output('graph2','figure'),
-    [dash.dependencies.Input('stock-ticker-input', 'value')])
-def update_graph2(tickers):
-    #graph2 = []
-    for i, ticker in enumerate(tickers):
-        try:
-            df = DataReader(str(ticker), 'morningstar',
-                            dt.datetime(2017, 1, 1),
-                            dt.datetime.now()).reset_index()
-        except:
-            graph2.append(html.H3(
-                'Data is not available for {}'.format(ticker),
-                style={'marginTop': 20, 'marginBottom': 20}
-            ))
-            continue
-        
-        volume = [{
-            'x': df['Date'],
-            'y': df['Volume'],
-            'legendgroup': ticker,
-            'type': 'bar',
-            'showlegend': True
-        }]
+    [dash.dependencies.Input('stock-ticker-input', 'value'),
+     dash.dependencies.Input('stock_timespan', 'value')])
+def update_graph2(tickers,stock_timespan):
+    
+    df = df_clean_prices.loc[tickers]
+    df = df.reset_index()  
 
-        # graph2.append(dcc.Graph(
-        #     id='graph2',
-        #     figure={
-        #         'data': [volume],
-        #         'layout': {
-        #             'margin': {'b': 80, 'r': 30, 'l': 60, 't': 0},
-        #             'legend': {'x': 0}
-        #         }
-        #     }
-        # ))
+    try:
+        days = int(stock_timespan[0])
+    except:
+        days = int(stock_timespan)
+
+    trace0 = go.Scatter(
+        x = df['date'][-days:],
+        y = df['close'][-days:],
+        mode = 'lines',
+        name = 'close')
+
+    trace1 = go.Scatter(
+        x = df['date'][-days:],
+        y = df['EMA_200'][-days:],
+        mode = 'lines',
+        name = '200day')     
+
+
+    trace2 = go.Scatter(
+        x = df['date'][-days:],
+        y = df['EMA_50'][-days:],
+        mode = 'lines',
+        name = '50day')   
+
+
+    data = [trace0, trace1, trace2]
 
     return{
-        'data': volume
+        'data': data,
+        'layout': {
+            'xaxis':{'rangeslider': {'visible': False}},
+            'title' : 'Moving Averages'
+        }
     }
 
-
+#---------------------------------------------------------------------------------------------------------------------------
+# Plot Graph 3 - Moving Averages
+#---------------------------------------------------------------------------------------------------------------------------
 
 @app.callback(
     dash.dependencies.Output('graph3','figure'),
-    [dash.dependencies.Input('stock-ticker-input', 'value')])
+    [dash.dependencies.Input('stock-ticker-input', 'value'),
+     dash.dependencies.Input('stock_timespan', 'value')])
+def update_graph3(tickers,stock_timespan):
     
-def update_graph3(tickers):
-    #graph2 = []
-    for i, ticker in enumerate(tickers):
-        try:
-            df = DataReader(str(ticker), 'morningstar',
-                            dt.datetime(2017, 1, 1),
-                            dt.datetime.now()).reset_index()
-        except:
-            graph3.append(html.H3(
-                'Data is not available for {}'.format(ticker),
-                style={'marginTop': 20, 'marginBottom': 20}
-            ))
-            continue
-        
-        volume = [{
-            'x': df['Date'],
-            'y': df['Volume'],
-            'legendgroup': ticker,
-            'type': 'bar',
-            'showlegend': True
-        }]
+    df = df_clean_prices.loc[tickers]
+    df = df.reset_index()  
+    #mean_volume = df['volume'].mean()
 
-        # graph2.append(dcc.Graph(
-        #     id='graph2',
-        #     figure={
-        #         'data': [volume],
-        #         'layout': {
-        #             'margin': {'b': 80, 'r': 30, 'l': 60, 't': 0},
-        #             'legend': {'x': 0}
-        #         }
-        #     }
-        # ))
+    try:
+        days = int(stock_timespan[0])
+    except:
+        days = int(stock_timespan)
+
+    trace1 = [go.Bar(
+                x = df['date'][-days:],
+                y = df['volume'][-days:]
+        )]
+
 
     return{
-        'data': volume
+        'data': trace1,
+        'layout': {
+            'xaxis':{'rangeslider': {'visible': False}},
+            'title' : 'Volume'
+        }
     }
+
+#---------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     app.run_server(debug=True)
