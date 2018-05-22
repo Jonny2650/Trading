7922b95e6b27c20356cdabb24f190c1f884471c9
Aaron Moss <mossaaron@outlook.com>
2018-05-22 11:14:57 +0100

    updates

--- app_aaron.py
+++ app_aaron.py
@@ -5,20 +5,55 @@ import dash_html_components as html
 import datetime as dt
 import pandas as pd
 from pandas_datareader.data import DataReader
+import plotly.plotly as py
 import plotly.graph_objs as go
+
 
 app = dash.Dash()
 
 
+external_css = [ "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
+        "//fonts.googleapis.com/css?family=Raleway:400,300,600",
+        "https://codepen.io/ygkubrick/pen/KoZPvz.css", #this is the header formatting
+        "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
+
+for css in external_css:
+    app.css.append_css({ "external_url": css })
+
+external_js = [ "https://code.jquery.com/jquery-3.2.1.min.js",
+        "https://codepen.io/plotly/pen/KmyPZr.js" ]
+
+for js in external_js:
+    app.scripts.append_script({ "external_url": js })
+
+#app.css.append_css({'external url': 'https://codepen.io/aaron-moss/pen/GdeQyK.css'})
+
+colors = {'background': '#ED3131'}
+
+df_table = pd.read_csv('test_table.csv')
 df_symbol = pd.read_csv('tickers.csv')
-app.config['suppress_callback_exceptions']=True	
 
-app.layout = html.Div([
+app.config['suppress_callback_exceptions']=True 
 
-	html.Div([
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}[
+    
+    html.Div([
     html.H2('Stock Explorer',
                 style={'display': 'inline',
-                       'float': 'left',
+                       'float': 'center',
                        'font-size': '2.65em',
                        'margin-left': '7px',
                        'font-weight': 'bolder',
@@ -33,7 +68,7 @@ app.layout = html.Div([
     className='row'
     ),
 
-	html.Div(
+    html.Div(
         [
         dcc.Dropdown(
             id='stock-ticker-input',
@@ -50,66 +85,32 @@ app.layout = html.Div([
     html.Div(
         className="row",
         children=[
+           
             html.Div(
-                className="six columns",
-                children=[
-                    html.Div(
-                    	children=dcc.Graph(id='graph1')
-
-                        )
-                    ]
+                className="eight columns",
+                children=html.Div([
+                    dcc.Graph(id='graph1'),
+                    dcc.Graph(id='graph2')
+       
+            ])
             ),
-               
             
             html.Div(
-                className="six columns",
+                className="four columns",
                 children=html.Div([
-                    dcc.Graph(
-                        id='right-top-graph',
-                        figure={
-                            'data': [{
-                                'x': [1, 2, 3],
-                                'y': [3, 1, 2],
-                                'type': 'bar'
-                            }],
-                            'layout': {
-                                'height': 400,
-                                'margin': {'l': 10, 'b': 20, 't': 0, 'r': 0}
-                            }
-                        }
-                    ),
-                    dcc.Graph(
-                        id='right-bottom-graph',
-                        figure={
-                            'data': [{
-                                'x': [1, 2, 3],
-                                'y': [3, 1, 2],
-                                'type': 'bar'
-                            }],
-                            'layout': {
-                                'height': 400,
-                                'margin': {'l': 10, 'b': 20, 't': 0, 'r': 0}
-                            }
-                        }
-                    ),
-
-                ])
+                    generate_table(df_table),
+                    dcc.Graph(id='graph3')
+            ])               
             ),
         ]
     ),
 ])
-
-app.css.append_css({
-    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-})
-
 
 
 @app.callback(
     dash.dependencies.Output('graph1', 'figure'),
     [dash.dependencies.Input('stock-ticker-input', 'value')])
 
-	
 def update_graph1(tickers):
     #print(ticker)
    
@@ -138,14 +139,138 @@ def update_graph1(tickers):
             'increasing': {'line': {'color': '#228B22'}},
             'decreasing': {'line': {'color': '#FF0000'}}
         }]
-		
+        
     return{
             'data': candlestick# + bollinger_traces
-			#x = [1,2,3,4,5],
-			#y = [1,2,3,4,5],
-			#mode = 'markers')]
+            #x = [1,2,3,4,5],
+            #y = [1,2,3,4,5],
+            #mode = 'markers')]
     }
+   
+
+
+@app.callback(
+    dash.dependencies.Output('graph4', 'figure'),
+    [dash.dependencies.Input('stock-ticker-input', 'value')])
+def update_graph1(tickers):
+    #print(ticker)
+   
+    for i, ticker in enumerate(tickers):
+        try:
+            df = DataReader(str(ticker), 'morningstar',
+                            dt.datetime(2017, 1, 1),
+                            dt.datetime.now()).reset_index()
+        except:
+            graph4.append(html.H3(
+                'Data is not available for {}'.format(ticker),
+                style={'marginTop': 20, 'marginBottom': 20}
+            ))
+            continue
+
+        candlestick = [{
+            'x': df['Date'],
+            'open': df['Open'],
+            'high': df['High'],
+            'low': df['Low'],
+            'close': df['Close'],
+            'type': 'candlestick',
+            #'name': 'graph1',
+            'name': ticker,
+            'legendgroup': ticker,
+            'increasing': {'line': {'color': '#228B22'}},
+            'decreasing': {'line': {'color': '#FF0000'}}
+        }]
+        
+    return{
+            'data': candlestick# + bollinger_traces
+            #x = [1,2,3,4,5],
+            #y = [1,2,3,4,5],
+            #mode = 'markers')]
+    }
+
+
+@app.callback(
+    dash.dependencies.Output('graph2','figure'),
+    [dash.dependencies.Input('stock-ticker-input', 'value')])
+def update_graph2(tickers):
+    #graph2 = []
+    for i, ticker in enumerate(tickers):
+        try:
+            df = DataReader(str(ticker), 'morningstar',
+                            dt.datetime(2017, 1, 1),
+                            dt.datetime.now()).reset_index()
+        except:
+            graph2.append(html.H3(
+                'Data is not available for {}'.format(ticker),
+                style={'marginTop': 20, 'marginBottom': 20}
+            ))
+            continue
+        
+        volume = [{
+            'x': df['Date'],
+            'y': df['Volume'],
+            'legendgroup': ticker,
+            'type': 'bar',
+            'showlegend': True
+        }]
+
+        # graph2.append(dcc.Graph(
+        #     id='graph2',
+        #     figure={
+        #         'data': [volume],
+        #         'layout': {
+        #             'margin': {'b': 80, 'r': 30, 'l': 60, 't': 0},
+        #             'legend': {'x': 0}
+        #         }
+        #     }
+        # ))
+
+    return{
+        'data': volume
+    }
+
+
+
+@app.callback(
+    dash.dependencies.Output('graph3','figure'),
+    [dash.dependencies.Input('stock-ticker-input', 'value')])
     
+def update_graph3(tickers):
+    #graph2 = []
+    for i, ticker in enumerate(tickers):
+        try:
+            df = DataReader(str(ticker), 'morningstar',
+                            dt.datetime(2017, 1, 1),
+                            dt.datetime.now()).reset_index()
+        except:
+            graph3.append(html.H3(
+                'Data is not available for {}'.format(ticker),
+                style={'marginTop': 20, 'marginBottom': 20}
+            ))
+            continue
+        
+        volume = [{
+            'x': df['Date'],
+            'y': df['Volume'],
+            'legendgroup': ticker,
+            'type': 'bar',
+            'showlegend': True
+        }]
+
+        # graph2.append(dcc.Graph(
+        #     id='graph2',
+        #     figure={
+        #         'data': [volume],
+        #         'layout': {
+        #             'margin': {'b': 80, 'r': 30, 'l': 60, 't': 0},
+        #             'legend': {'x': 0}
+        #         }
+        #     }
+        # ))
+
+    return{
+        'data': volume
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
